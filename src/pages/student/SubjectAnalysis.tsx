94d567038be1712ec5cfedd79a5200a01e34172d
halil.adiyaman <halilay45@gmail.com>
2025-08-16 14:47:49 +0300

    Fix mobile responsive design for subject cards - single column on phones

--- src/pages/student/SubjectAnalysis.tsx
+++ src/pages/student/SubjectAnalysis.tsx
@@ -346,7 +346,7 @@ const SubjectAnalysis: React.FC = () => {
         </div>
         
         {/* Subjects Grid */}
-        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
           {subjects.map((subject, index) => (
             <motion.div
               key={subject.id}
